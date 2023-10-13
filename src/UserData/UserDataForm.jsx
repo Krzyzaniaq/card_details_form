42d7bc670c83f0d8d6d516bdf625a019ebe0f2a9
krzyzaniaq <kamil.krzyzaniak23@gmail.com>
2023-10-13 17:08:30 +0200

    Styles for UserDataForm/Button

--- src/UserData/UserDataForm.jsx
+++ src/UserData/UserDataForm.jsx
@@ -3,44 +3,60 @@ import classes from './UserDataForm.module.css';
 const UserDataForm = () => {
   return (
     <div className={classes['data-form']}>
-      <p className={classes.text}>Cardholder name</p>
-      <input
-        type="text"
-        className={classes['data-input']}
-        id="user-data-name"
-        placeholder="e.g. Jane Appleseed"
-      />
-      <p className={classes.text}>Card number</p>
-      <input
-        type="text"
-        className={classes['data-input']}
-        id="user-data-number"
-        placeholder="e.g. 1234 5678 9123 0000"
-      />
-      <p className={classes.text}>exp. date (MM/YY)</p>
-      <input
-        type="number"
-        className={classes['data-input']}
-        id="user-data-month"
-        placeholder="MM"
-      />
-      <input
-        type="number"
-        className={classes['data-input']}
-        id="user-data-year"
-        min="2023"
-        max="2030"
-        placeholder="YY"
-      />
-      <p className={classes.text}>CVC</p>
-      <input
-        type="number"
-        className={classes['data-input']}
-        id="user-data-cvc"
-        min="1"
-        max="12"
-        placeholder="e.g. 123"
-      />
+      <span>
+        <h4 className={classes.text}>CARDHOLDER NAME</h4>
+        <input
+          type="text"
+          className={classes['data-input']}
+          id="user-data-name"
+          placeholder="e.g. Jane Appleseed"
+        />
+      </span>
+      <span>
+        <h4 className={classes.text}>CARD NUMBER</h4>
+        <input
+          type="text"
+          className={classes['data-input']}
+          id="user-data-number"
+          placeholder="e.g. 1234 5678 9123 0000"
+        />
+      </span>
+
+      <div className={classes['data-date-cvc']}>
+        <div className={classes['data-date-inputs']}>
+          <h4 className={classes.text}>EXP. DATE M/Y</h4>
+          <div>
+            <input
+              type="text"
+              className={classes['data-input']}
+              id="user-data-month"
+              placeholder="MM"
+              maxLength="2"
+            />
+            <input
+              type="text"
+              className={classes['data-input']}
+              id="user-data-year"
+              min="2023"
+              max="2030"
+              maxLength="2"
+              placeholder="YY"
+            />
+          </div>
+        </div>
+        <div className={classes['data-cvc-input']}>
+          <h4 className={classes.text}>CVC</h4>
+          <input
+            type="text"
+            className={classes['data-input']}
+            id="user-data-cvc"
+            min="1"
+            max="12"
+            maxLength="3"
+            placeholder="e.g. 123"
+          />
+        </div>
+      </div>
     </div>
   );
 };
