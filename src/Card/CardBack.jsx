cd4bd93b5099f4f45e8b11f48a3aa061dade4fec
krzyzaniaq <kamil.krzyzaniak23@gmail.com>
2023-10-13 17:19:18 +0200

    Cards styles

--- src/Card/CardBack.jsx
+++ src/Card/CardBack.jsx
@@ -3,7 +3,7 @@ import classes from './CardBack.module.css';
 const CardBack = () => {
   return (
     <div className={classes['card-back']}>
-      <p className={classes['card-back-cvc']}>dfsfsfs</p>
+      <p className={classes['card-back-cvc']}>000</p>
     </div>
   );
 };
--- src/Card/CardFront.jsx
+++ src/Card/CardFront.jsx
@@ -6,10 +6,10 @@ const CardFront = () => {
     <div className={classes['card-front']}>
       <img src={cardLogo} className={classes['card-logo']} alt="card logo" />
       <div className={classes['card-container']}>
-        <h3 className={classes['card-number']}>9591 6489 6389 1011</h3>
+        <h3 className={classes['card-number']}>0000 0000 0000 0000</h3>
         <div className={classes['card-bottom']}>
-          <p className={classes['card-name']}>dssa asdasdas</p>
-          <p className={classes['card-date']}>09/26</p>
+          <p className={classes['card-name']}>JANE APPLESEED</p>
+          <p className={classes['card-date']}>00/00</p>
         </div>
       </div>
     </div>
