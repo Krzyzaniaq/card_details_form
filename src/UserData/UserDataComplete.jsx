ab0c4e9ea8bdc76a16267389739d72f5fe63e352
krzyzaniaq <kamil.krzyzaniak23@gmail.com>
2023-10-16 19:41:58 +0200

    UserDataComplete styles

--- src/UserData/UserDataComplete.jsx
+++ src/UserData/UserDataComplete.jsx
@@ -1,18 +1,18 @@
-import classes from './UserDataComplete.module.css';
 import Button from '../UI/Button';
 
+import classes from './UserDataComplete.module.css';
 import iconComplete from '../assets/icon-complete.svg';
 
 const UserDataComplete = (props) => {
   return (
-    <>
+    <div className={classes['data-complete']}>
       <img src={iconComplete} alt="Icon showing completed sign" />
       <h2>THANK YOU!</h2>
-      <p>We've added your card details</p>
+      <p>We've added your card details.</p>
       <Button type="button" onClick={props.onContinue}>
         Continue
       </Button>
-    </>
+    </div>
   );
 };
 
